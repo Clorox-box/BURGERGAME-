782fd45ab3f24dc8b4f503e2b07b76ec04ee388c
Clorox-box <25igbokweml@marshallisd.com>
2023-02-22 10:01:46 -0600

    added player movement (+ miku)

--- BURGERBUSTERS/Assets/PlayerMovement.cs
+++ BURGERBUSTERS/Assets/PlayerMovement.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-Rigidbody2D body;
+public class playermovement : MonoBehaviour
+{
 
-float horizontal; 
-float
+public float moveSpeed = 5f;
+public Rigidbody2D rb;
+Vector2 movement;
 
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
